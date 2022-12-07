e5e452108592fe7558b6654cf4f121f5ee2277d8
ogawa.seiya <ogawa.seiya.pri@gmail.com>
2022-12-08 01:35:40 +0900

    テスト用API作成

---
+++ src/services/get-pokemon.ts
@@ -0,0 +1,8 @@
+import type { Pokemon } from 'types';
+import { fetcher } from 'utils';
+
+const getPokemons = () => {
+    return [{ id: 1, name: test, firstType: 1, secondType: 2 }];
+};
+
+export default getPokemons;
---
+++ src/types/data.d.ts
@@ -0,0 +1,13 @@
+// ポケモン
+export Pokemon = {
+  id: number,
+  name: string,
+  firstType: number,
+  secondType: number
+}
+
+// ポケモンのタイプ
+export PokemonType = {
+  id: number,
+  name: string
+}
