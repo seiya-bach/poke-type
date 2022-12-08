347d1633f495de9230bff0752059f0c9d0fafede
Seiya <ogawa.seiya.pri@gmail.com>
2022-12-08 18:51:24 +0900

    ロゴコンポーネントの追加 (#14)

---
+++ src/components/atoms/Logo/index.stories.tsx
@@ -0,0 +1,5 @@
+import { ComponentMeta } from "@storybook/react";
+import AppLogo from './index';
+
+export default { title: 'Atoms/Logo' } as ComponentMeta<typeof AppLogo>
+export const Logo = () => <AppLogo />
