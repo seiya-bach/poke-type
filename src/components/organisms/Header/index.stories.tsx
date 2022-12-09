b0f1ae04680902551eec54ebddb60a86910aae6b
Seiya <ogawa.seiya.pri@gmail.com>
2022-12-09 19:24:35 +0900

    ヘッダーを追加 (#19)
    * ヘッダーを追加
    
    * フォーマット

---
+++ src/components/organisms/Header/index.stories.tsx
@@ -0,0 +1,8 @@
+import { ComponentMeta } from '@storybook/react';
+import Header from './index';
+
+export default {
+    title: 'Organisms/Header',
+} as ComponentMeta<typeof Header>;
+
+export const Normal = () => <Header />;
---
+++ src/components/organisms/Header/index.tsx
@@ -0,0 +1,25 @@
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import * as React from 'react';
+import Logo from 'components/atoms/Logo';
+
+export default function Header() {
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <Logo />
+                    <Typography
+                        variant="h6"
+                        component="div"
+                        sx={{ flexGrow: 1 }}
+                    >
+                        ポケモンのタイプが覚えられない.com
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
+}
